0ad8b34f6ebaddfdea7e2e5d0753f882c0ca1f79
Meghan Khurana <mkhurana@fb.com>
2017-05-06 14:45:38 -0700

    create board and initialize to zero

---
+++ project/connect_four.py
@@ -0,0 +1,44 @@
+"""
+Connect Four game
+
+about/instructions
+"""
+
+#print "Hello, World!"
+board = []
+cols = 7
+rows = 6
+
+def create_board():
+    # create a new board and initialize all spaces to zero
+
+    for i in range(cols):
+        for j in range(rows):
+            board.append(0)
+
+    return board
+
+
+
+#    print board
+
+def display_board(board):
+    # display the board
+
+#    print "\n"
+#    print "\n".join(str(board[:]))
+#    print "\n"
+
+#    print str(board)
+
+    for row in board:
+#        print " ".join(str(board[i]))
+        if i % cols == 0:
+            print '\n'
+        print board[i]
+#    print('\n'.join([''.join(['{:}'.format(item) for str(item) in row])
+#      for row in board]))
+
+
+create_board()
+display_board(board)
