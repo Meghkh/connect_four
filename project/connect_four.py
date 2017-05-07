9538652449e9192dde9ebee44e69eaf100a0a301
Meghan Khurana <mkhurana@fb.com>
2017-05-06 17:15:50 -0700

    board takes user input

--- project/connect_four.py
+++ project/connect_four.py
@@ -15,6 +15,8 @@ board = []
 cols = 7
 rows = 6
 
+player_flag = 1
+
 def create_board():
     """ create a new board and initialize all spaces to zero
 
@@ -34,25 +36,51 @@ def create_board():
 
     return board
 
+
+
 def display_board(board):
     """ display the board
+        include row and column identifiers to help users select move
 
-        decided to separate the displaying of the board from the creating
-        of a new board so that it will be easier to later implement a new
-        game function.
+        separate the displaying of the board from the creating
+        of a new board so that board can be viewed without
+        re-initializing
     """
 
     print "\n"
+    print "\tROWS\n\t\tCOLUMNS"
+#    print "\t\t\tCOLUMNS"
     for row in board:
 #        print " ".join(str(row))
-        print "\t\t\t" + str(row)
+        print "\t\t", "", "\t", str(row)
     print "\n"
 
-#def input_next_move(board):
+
+
+def input_next_move(board, player_flag):
     """ input coordinates for next move based on user input
 
     """
 
+    print "Select coordinates for next move, Player", player_flag, ":\n"
+    row_coordinate = int(raw_input(" Which row? "))
+    col_coordinate = int(raw_input(" Which col? "))
+
+    if board[row_coordinate-1][col_coordinate-1] != 0:
+        print "Coordinate is occupied! Try again!"
+    else:
+        board[row_coordinate - 1][col_coordinate - 1] = player_flag
+        # switch player_flag only when valid move is made
+        if player_flag == 1:
+            player_flag += 1
+        else:
+            player_flag -= 1
+
+    display_board(board)
+
+
+
+    input_next_move(board, player_flag)
 
 
 """
@@ -62,3 +90,4 @@ def display_board(board):
 
 create_board()
 display_board(board)
+input_next_move(board, player_flag)
